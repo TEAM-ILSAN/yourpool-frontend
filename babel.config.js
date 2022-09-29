feffc47ba922b2f2afc356b832c4872957c26522
doyoonear <doyoonleee@gmail.com>
2022-09-29 23:30:36 +0900

    feature: Add types dependency

--- babel.config.js
+++ babel.config.js
@@ -18,5 +18,25 @@ module.exports = {
         verbose: false,
       },
     ],
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
   ],
 };
