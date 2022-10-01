7cf7635ed5dc9107b846cb6fa84dc06015352623
jjung <iamhojjung@gmail.com>
2022-10-01 15:55:20 +0900

    component title 이동

--- src/components/common/Typography/Title.tsx
+++ src/components/common/Typography/Title.tsx
@@ -1,13 +1,16 @@
-import { ReactChild } from 'react';
-import styled from '@emotion/styled';
-import { font } from '@/styles/theme';
+import styled from '@emotion/native';
+import {font} from '@/styles/theme';
 
-const Title = ({ children }: { children: ReactChild }): JSX.Element => {
-  return <StyledTitle>{children}</StyledTitle>;
+interface TitleProps {
+  title: string;
+}
+
+const Title = ({title}: TitleProps): JSX.Element => {
+  return <StyledTitle>{title}</StyledTitle>;
 };
 
 // TODO: 실제 사용되는 타이틀 사이즈, 스타일로 변경
-const StyledTitle = styled.div`
+const StyledTitle = styled.Text`
   font-size: ${font.size22};
 `;
 
