834c6a7322fbec637c4f757cb06ec4f5f1ea141c
doyoonear <doyoonleee@gmail.com>
2022-10-03 23:47:55 +0900

    feature: Move style, constants, assets icons for import

---
+++ src/styles/theme.ts
@@ -0,0 +1,46 @@
+const font = {
+  size16: '1.6rem',
+  size18: '1.8rem',
+  size22: '2.2rem',
+  size80: '8rem',
+};
+
+const color = {
+  primary: '#A9ECE0',
+  white: '#FFF',
+  black: '#000',
+  lightgrey: '#F6F6F6',
+  darkgrey: '#605A5A',
+};
+
+const gutter = {
+  size8: '0.8rem',
+  size12: '1.2rem',
+  size16: '1.6rem',
+  size20: '2rem',
+  size24: '2.4rem',
+  size28: '2.8rem',
+  size32: '3.2rem',
+  size36: '3.6rem',
+  size40: '4rem',
+  size56: '5.6rem',
+  size64: '6.4rem',
+  size84: '8.4rem',
+  size88: '8.8rem',
+  size136: '13.6rem',
+};
+
+const zIndexes = {
+  default: 1,
+  floor: 10,
+  cloud: 100,
+  space: 1000,
+};
+
+const borderRadiuses = {
+  small: '0.2rem',
+  large: '0.4rem',
+  circle: '100%',
+};
+
+export { font, color, gutter, zIndexes, borderRadiuses };
