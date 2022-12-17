f0c24df9925798e606fbfaaed23b530fa9d36eab
이조은 <joyfuljoeunlee@gmail.com>
2022-12-17 16:19:13 +0900

    feature: layout 컴포넌트 react native 코드 변환

--- src/components/common/Layouts/index.tsx
+++ src/components/common/Layouts/index.tsx
@@ -1,19 +1,21 @@
 import { PropsWithChildren } from 'react';
-import styled from '@emotion/styled';
+import styled from '@emotion/native';
 import { color } from '@/styles/theme';
 
 interface LayoutProps {
   bgColor?: string;
 }
 
-function Layout({ children, bgColor }: PropsWithChildren<LayoutProps>) {
+function Layout({
+  children,
+  bgColor = color.primary,
+}: PropsWithChildren<LayoutProps>) {
   return <StyledLayout bgColor={bgColor}>{children}</StyledLayout>;
 }
 
-const StyledLayout = styled.div<{ bgColor: string }>`
-  height: calc(100vh - 6rem);
+const StyledLayout = styled.View<{ bgColor: string }>`
   padding: 3.6rem 2rem;
-  background-color: ${({ bgColor }) => (bgColor ? bgColor : color.primary)};
+  background-color: ${({ bgColor }) => bgColor};
 `;
 
 export default Layout;
